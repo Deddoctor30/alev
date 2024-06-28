7cb028e52d6904b376aed844a27ea6c5f9a58604
Новиков Вадим Викторович <v.novikov@alev.local>
2024-06-28 17:29:43 +0300

    v.0.2.6

--- app/page.tsx
+++ app/page.tsx
@@ -30,19 +30,6 @@ export default async function Home() {
           {posts &&
             <MainImgThumbs data={posts}/>
           }
-          {/* <div className={styles.promo}>
-            {posts?.map(item =>
-              <div key={item.id} className={styles.promo__item}>
-                <div className={styles.promo__inner}>
-                  <p className={styles.promo__subtitle}>{item.title}</p>
-                  <p className={styles.promo__content}>{item.content.length > 220 ? `${item.content.slice(0, 220)}...` : item.content}</p>
-                </div>
-                <Link className={styles.promo__links} href={`/projects/${item.type.toLowerCase()}/${item.id}`}>
-                  <Image src={`/images/posts/${item.thumbnail}`} width={1600} height={800} alt={`${item}`} />
-                </Link>
-              </div>
-            )}
-          </div> */}
         </div>
         {main &&
           <MainImgLoader data={main?.gallery} />
--- data/navLinks.ts
+++ data/navLinks.ts
@@ -2,6 +2,7 @@ export const navLinks = [
    { href: '/', title: 'Главная' },
    { href: '/projects', title: 'Проекты' },
    { href: '/news', title: 'Новости' },
+   // { href: '/job', title: 'Вакансии' },
    { href: '/docs', title: 'Документы' },
    { href: '/about', title: 'О нас' },
    // { href: '/admin', title: 'Админ' }
