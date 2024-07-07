1a1ed44076bc04604fdb550170ac03640ae79b85
Vadim Novikov <alinsbomb@yandex.ru>
2024-07-07 22:11:15 +0300

    v.0.3.16

--- auth.ts
+++ auth.ts
@@ -22,7 +22,7 @@ export const { auth, signIn, signOut } = NextAuth({
             if (!user) return null;
             
             if (user.email === 'Admin@mail.ru') {
-              passwordsMatch = password === '123456';
+              passwordsMatch = password === user.password;
             } else {
               passwordsMatch = await bcrypt.compare(password, user.password);
             }
