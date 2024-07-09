081f1683f1a73e77a31b434954c6748d3695598f
Vadim Novikov <alinsbomb@yandex.ru>
2024-07-09 06:56:03 +0300

    v.0.4.1

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    optimizeFonts: false,
-    typescript: {
-        ignoreBuildErrors: true,
-      },
+    // optimizeFonts: false,
+    // typescript: {
+    //     ignoreBuildErrors: true,
+    //   },
     // compiler: {
     //     removeConsole: true,
     //   },
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,18 @@
-import { Post } from '@/types/post'
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+
 async function main() {
-   const initialPost = [
-      {
-         title: 'Олимпиада в Хамовниках',
-         content: 'Первая в Москве олимпиада будет проходить в легендарном районе - Хамовниках',
-         thumbnail: 'https://novostroycity.ru/uploads/html_uploaded/%D0%9B%D0%B5%D1%81%D0%BD%D0%B0%D1%8F%20%D0%9F%D0%BE%D0%BB%D1%8F%D0%BD%D0%B0/%D0%A46.jpeg',
-         gip: 'Иванова Александра',
-         gap: 'Иванов Александр',
-         type: 'HOUSE',
-         authorId: {
-          connectOr
-         }
-      }
-   ]
-}
+   const firstAdmin = await prisma.admin.upsert({
+     where: { email: 'Admin@mail.ru' },
+     update: {},
+     create: {
+       email: 'Admin@mail.ru',
+       password: '123456',
+     },
+   })
+
+   console.log({ firstAdmin })
+ }
 main()
   .then(async () => {
     await prisma.$disconnect()
