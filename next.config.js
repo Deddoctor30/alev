ebae1fb123fa88359aeec5b336147d50c0f350da
Vadim Novikov <alinsbomb@yandex.ru>
2024-08-12 21:44:23 +0300

    v.1.1.0 add docker presets

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     // optimizeFonts: false,
+
+    // Для Dockera раскомитить ниже, собрает файлы в одну папку
+    // output: 'standalone',
     typescript: {
         ignoreBuildErrors: true,
       },
